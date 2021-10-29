e56b92334604aaacb583c95357f32f2ca83be5e8
Bruno PINTO <brpinto@e3r9p11.42.fr>
2021-10-29 17:36:38 +0200

    add finish to end activity on contact creation

--- app/src/main/java/com/example/ft_hangouts/ContactForm.kt
+++ app/src/main/java/com/example/ft_hangouts/ContactForm.kt
@@ -1,11 +1,21 @@
 package com.example.ft_hangouts
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class ContactForm : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contact_form)
+
+        val create_contact = findViewById<Button>(R.id.create_contact)
+
+        create_contact.setOnClickListener(){
+            val main_intent = Intent(this, MainActivity::class.java).apply{}
+
+            finish()
+        }
     }
 }
