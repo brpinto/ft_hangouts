ff8acc654fe27f76ab61c4849dc82556392a0b15
brpinto <brpinto@student.42.fr>
2022-04-13 00:27:08 +0200

    delete recreate function call

--- app/src/main/java/com/example/ft_hangouts/MainActivity.kt
+++ app/src/main/java/com/example/ft_hangouts/MainActivity.kt
@@ -16,7 +16,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        recreate()
         var toolbar = findViewById<Toolbar>(R.id.my_toolbar)
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayShowTitleEnabled(false);
